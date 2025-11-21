4b38236b462750a28e754742333e104617a20c28
Psuedoo <forthewin720@gmail.com>
2025-11-20 19:25:50 -0500

    upload

--- app/components/card-finder.tsx
+++ app/components/card-finder.tsx
@@ -1,5 +1,24 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Input } from "@/components/ui/input";
+import { CopyButton } from "@/components/ui/shadcn-io/copy-button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Textarea } from "@/components/ui/textarea";
 import { useState } from "react";
 
 export default function CardFinder() {
@@ -23,9 +42,7 @@ export default function CardFinder() {
         return { count, name };
       })
       .forEach((card) => {
-        const res = fetch(
-          `/api/card?name=${encodeURIComponent(card.name.trim())}`,
-        )
+        fetch(`/api/card?name=${encodeURIComponent(card.name.trim())}`)
           .then((res) => res.json())
           .then((data: any) => {
             setCards((prev) => [...prev, data]);
@@ -33,48 +50,167 @@ export default function CardFinder() {
       });
   };
 
+  function splitCards(cards: any[]) {
+    return {
+      proxy: cards.filter((card: any) => {
+        const usd = parseFloat(card.prices?.usd ?? "0");
+        return usd >= 1;
+      }),
+
+      real: cards.filter((card: any) => {
+        const usd = parseFloat(card.prices?.usd ?? "0");
+        return usd < 1;
+      }),
+    };
+  }
+
+  function getTotalCost(cards: any[]) {
+    return cards.reduce((acc, card) => {
+      const priceStr = card.prices?.usd;
+      const price = priceStr ? parseFloat(priceStr) : 0;
+
+      if (Number.isNaN(price)) return acc;
+      return acc + price;
+    }, 0);
+  }
+
+  function getTCGName(card: any) {
+    // Base name: strip split/DFC second face for Mass Entry
+    // const baseName = (card.name ?? "").split("//")[0].trim();
+    const baseName = (card.name ?? "").trim();
+    let name = baseName;
+
+    // Border types
+    switch (card.border_color) {
+      case "borderless":
+        name += " (Borderless)";
+        break;
+    }
+
+    // Frame effects (extended, showcase, retro, etc.)
+    // const effects: string[] = card.frame_effects ?? [];
+
+    // if (effects.includes("extended")) {
+    //   name += " (Extended Art)";
+    // }
+    // if (effects.includes("showcase")) {
+    //   name += " (Showcase)";
+    // }
+    // if (effects.includes("retro")) {
+    //   name += " (Retro Frame)";
+    // }
+    // if (effects.includes("inverted")) {
+    //   name += " (Inverted)";
+    // }
+    // Etched foil
+    // if (card.finishes?.includes("etched")) {
+    //   name += " (Etched Foil)";
+    // }
+
+    return name;
+  }
+
+  const textCardList = cards
+    .map((card: any) => {
+      const tcgName = getTCGName(card);
+      const count = card.count ?? 1;
+      return `${count} ${tcgName}`;
+    })
+    .join("\n");
+
+  // 1 Witch Enchanter // Witch-Blessed Meadow
+
   return (
-    <div className="flex flex-col h-15 justify-between items-start">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
+    <div className="flex flex-col w-full justify-between items-start gap-2">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-2">
+        <Textarea
           value={cardList}
           onChange={(e) => setCardList(e.target.value)}
           className="border rounded-sm h-20"
-          placeholder="Card Name"
+          placeholder="List of cards here"
         />
-        <button className="border rounded-sm" type="submit">
-          Submit
-        </button>
+        <Button>Submit</Button>
       </form>
-      <div className="flex flex-wrap w-full">
-        {cards?.map((card) => (
-          <Card key={card.id} card={card} />
+      <div className="hidden">
+        <p>Total Price: ${Math.round(getTotalCost(cards))}</p>
+        <h1>
+          Total Real TCG Player Cost: $
+          {getTotalCost(splitCards(cards).real).toFixed(2)}
+        </h1>
+        <h3>Amount of cards: {splitCards(cards).real.length}</h3>
+        <h1>
+          Total Not Buying TCG Cost: $
+          {getTotalCost(splitCards(cards).proxy).toFixed(2)}
+        </h1>
+        <h3>Amount of cards: {splitCards(cards).proxy.length}</h3>
+      </div>
+      <div className="flex w-full flex-col gap-10">
+        <Tabs defaultValue="real">
+          <TabsList>
+            <TabsTrigger value="real" disabled={cards.length <= 0}>
+              Real
+            </TabsTrigger>
+            <TabsTrigger value="proxy" disabled={cards.length <= 0}>
+              Proxy
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="real">
+            <CardCarousel cards={splitCards(cards).real} />
+          </TabsContent>
+          <TabsContent value="proxy">
+            <CardCarousel cards={splitCards(cards).proxy} />
+          </TabsContent>
+        </Tabs>
+      </div>
+
+      <div className="w-full border rounded-sm mt-5">
+        <CopyButton content={textCardList} />
+        {cards.map((card, index) => (
+          <p key={index}>1 {card.name}</p>
         ))}
       </div>
     </div>
   );
 }
 
-function Card(props: any) {
+function CardCarousel(props: any) {
+  return (
+    <Carousel className="w-full">
+      <CarouselContent>
+        {props.cards?.map((card: any, index: any) => (
+          <CarouselItem
+            key={index}
+            className="flex items-center justify-center sm:basis-1/2 md:basis-1/4 lg:basis-1/5"
+          >
+            <div className="p-1">
+              <MagicCard card={card} />
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+}
+
+function MagicCard(props: any) {
   const card = props.card;
 
   const image =
     card.image_uris?.small ?? card.card_faces?.[0]?.image_uris?.small ?? null;
 
   return (
-    <div className="flex flex-col border rounded-sm p-2 w-40 h-80">
-      <div className="flex h-1/3 border">
-        <h1>{card.name}</h1>
-      </div>
-      <div className="flex flex-col p-5">
+    <Card className="overflow-hidden">
+      <CardHeader>
+        <CardTitle>{card.name}</CardTitle>
+      </CardHeader>
+      <CardContent className="flex items-center">
         <img width={150} src={image} />
-      </div>
-      <div>
-        {card.prices?.usd && (
-          <p className="text-gray-700">Scryfall Price: ${card.prices.usd}</p>
-        )}
-      </div>
-    </div>
+      </CardContent>
+      <CardFooter>
+        <p>${card.prices.usd}</p>
+      </CardFooter>
+    </Card>
   );
 }
